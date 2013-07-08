7d3e63c566fae2902a2421f7fe3b76728d01b940
Markus Török <markus.toeroek@gmail.com>
2013-07-08 15:06:35 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Speiseplan::Application.config.secret_token = '5b437ba8a535c7e3cc222e07a0c3bd828ab4837f75e30ee9b9d3d0849a9b4ddb603a2d46699605c03a95cc916ae7e78b13d83c41890cb34a83fb2bced6b078a5'
